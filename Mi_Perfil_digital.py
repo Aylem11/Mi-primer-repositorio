2ea372c742792c9e276995e8fea2890e2e6324dc
Aylem11 <aylem.asprilla1128@gmail.com>
2026-05-05 14:42:04 -0500

    Add Mi_Perfil_digital.py with personal data and playlist

---
+++ Mi_Perfil_digital.py
@@ -0,0 +1,27 @@
+#Uso de los tipos de datos en python
+
+#1 Datos basicos (str,int,bool,float)
+nombre= "Aylem sofia Asprilla meneses"
+edad= 14
+estatura= 1.58
+es_estudiante= "true"
+
+#2 Redes_sociales(tuple)
+redes_sociales= ("Aylem2011", "a_msvy")
+
+#3 Playlist de canciones favoritas (list en un dict)
+playlist= [{"titulo":"perfecta","artista":"miranda!","duración":3.45}]
+playlist= [{"titulo":"machu picchu","artista":"camilo,evaluna","duración":2.59}]
+playlist= [{"titulo":"ay mi dios","artista":"IAmChino,pitbull,yandel,El Chacal,","duración":4.12}]
+
+print("presentación personal")
+print("mi nombre es:",nombre)
+print("edad:",edad)
+print("mi estatura:"estatura)
+print("estoy activo en el colegio?",es_estudiante)
+print("mi playlist favorita")
+print(f"{canción["título"]}-{canción["artista"]}({canción["duración"]}"min")
+      
+print("_________")      
+
+           
